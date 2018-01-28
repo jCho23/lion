0e250e27d367a0b412cb3174f98e125bb34a8e45
June Cho <junecho.ucb@gmail.com>
2018-01-28 12:44:11 -0800

    Ienumerable change

--- lion/Views/HomeView.xaml.cs
+++ lion/Views/HomeView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using lion.Models;
 using Xamarin.Forms;
 
@@ -7,13 +8,24 @@ namespace lion.Views
 {
     public partial class HomeView : TabbedPage
     {
-        List<FeedDetails> GetPosts()
+        void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<FeedDetails> GetPosts(string searchText= null)
 		{
 			//Call to a remote service, placeholder for now
-			return new List<FeedDetails>{
+			var posts= new List<FeedDetails>{
                 new FeedDetails {Post= "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. ", UserUrl = "http://lorempixel.com/100/100/people/1"},
                 new FeedDetails { Post = "And the only way to do great work is to love what you do. ", UserUrl = "http://lorempixel.com/100/100/people/4", Status= "Hiya!" } 
             };
+
+            if (String.IsNullOrWhiteSpace(searchText))
+                return posts;
+            
+
+            return posts.Where(x => x.Post.StartsWith(searchText));
 		}
 		
 		void Handle_Refreshing(object sender, System.EventArgs e)
