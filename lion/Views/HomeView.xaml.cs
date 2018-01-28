2ffb2fba60ccc9fac726745eea091e90224f167e
June Cho <junecho.ucb@gmail.com>
2018-01-28 12:21:12 -0800

    CleanUp

--- lion/Views/HomeView.xaml.cs
+++ lion/Views/HomeView.xaml.cs
@@ -7,32 +7,26 @@ namespace lion.Views
 {
     public partial class HomeView : TabbedPage
     {
+        List<FeedDetails> GetPosts()
+		{
+			//Call to a remote service, placeholder for now
+			return new List<FeedDetails>{
+				new FeedDetails {Post= "1", UserUrl = "http://lorempixel.com/100/100/people/1"},
+				new FeedDetails { Post = "2", UserUrl = "http://lorempixel.com/100/100/people/4", Status= "Hiya!" } 
+            };
+			
+		}
+		
+		void Handle_Refreshing(object sender, System.EventArgs e)
+		{
+            listViewFeed.ItemsSource = GetPosts();
+		}
         public HomeView()
         {
             InitializeComponent();
 
-            //var posts = new List<string>
-            //{
-            //    "Post1",
-            //    "Post2",
-            //    "Post3"
-            //};
-            //listViewFeed.ItemsSource = posts;
-
-            listViewFeed.ItemsSource = new List<FeedDetails>{
-                new FeedDetails {Post= "1", UserUrl = "http://lorempixel.com/100/100/people/1"},
-                new FeedDetails { Post = "2", UserUrl = "http://lorempixel.com/100/100/people/4", Status= "Hiya!" } };
-
+            listViewFeed.ItemsSource = GetPosts();
         }
 
-        List<Post> GetPosts()
-        {
-            //Call to a remote service, placeholder for now
-        }
-
-        void Handle_Refreshing(object sender, System.EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
