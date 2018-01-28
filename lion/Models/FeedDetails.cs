8fff7c25687e6b5619bad08bb409e19087225b13
June Cho <junecho.ucb@gmail.com>
2018-01-28 11:27:04 -0800

    set properties

--- lion/Models/FeedDetails.cs
+++ lion/Models/FeedDetails.cs
@@ -3,6 +3,8 @@ namespace lion.Models
 {
     public class FeedDetails
     {
-         
+        public string Post { get; set; }
+        public string User { get; set; }
+        public string Status { get; set; }
     }
 }
