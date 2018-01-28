3fbf2431e38947e83470b4d50b593911d36ad2d0
June Cho <junecho.ucb@gmail.com>
2018-01-28 11:17:37 -0800

    cleanUp

---
+++ lion/Views/HomeView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace lion.Views
+{
+    public partial class HomeView : TabbedPage
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+
+            var posts = new List<string>
+            {
+                "Post1",
+                "Post2",
+                "Post3"
+            };
+
+            ListView.ItemsSource = posts;
+        }
+    }
+}
