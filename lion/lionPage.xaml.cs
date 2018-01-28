9bbd831ad6987063061fe02e68841fafa2407340
June Cho <junecho.ucb@gmail.com>
2018-01-28 08:47:58 -0800

    Initial Commit

---
+++ lion/lionPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace lion
+{
+    public partial class lionPage : ContentPage
+    {
+        public lionPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
