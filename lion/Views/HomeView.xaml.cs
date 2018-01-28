28e03eb5f822a304ed92848d1e5e9b5df3863666
June Cho <junecho.ucb@gmail.com>
2018-01-28 12:39:08 -0800

    CleanUp

--- lion/Views/HomeView.xaml.cs
+++ lion/Views/HomeView.xaml.cs
@@ -11,8 +11,8 @@ namespace lion.Views
 		{
 			//Call to a remote service, placeholder for now
 			return new List<FeedDetails>{
-				new FeedDetails {Post= "1", UserUrl = "http://lorempixel.com/100/100/people/1"},
-				new FeedDetails { Post = "2", UserUrl = "http://lorempixel.com/100/100/people/4", Status= "Hiya!" } 
+                new FeedDetails {Post= "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. ", UserUrl = "http://lorempixel.com/100/100/people/1"},
+                new FeedDetails { Post = "And the only way to do great work is to love what you do. ", UserUrl = "http://lorempixel.com/100/100/people/4", Status= "Hiya!" } 
             };
 		}
 		
