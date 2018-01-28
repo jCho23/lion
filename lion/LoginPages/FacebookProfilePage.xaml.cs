1b2f88ad81c766c85c3b6a411a9fc7d109c78e7b
June Cho <junecho.ucb@gmail.com>
2018-01-28 14:17:10 -0800

    Added FacebookProfilePage

---
+++ lion/LoginPages/FacebookProfilePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace lion.LoginPages
+{
+    public partial class FacebookProfilePage : ContentPage
+    {
+        public FacebookProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
