4b3efa1846559df3e25674aac683e7c998088568
June Cho <junecho.ucb@gmail.com>
2018-01-28 13:11:42 -0800

    PostsDetailsPage

---
+++ lion/Views/FormsDetailsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace lion.Views
+{
+    public partial class FormsDetailsView : ContentPage
+    {
+        public FormsDetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
