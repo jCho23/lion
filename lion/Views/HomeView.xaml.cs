7958c6a8488e17b18ffd687d0dba4bb96bf4043d
June Cho <junecho.ucb@gmail.com>
2018-02-08 18:33:37 -0800

    cleanup

--- lion/Views/HomeView.xaml.cs
+++ lion/Views/HomeView.xaml.cs
@@ -13,7 +13,7 @@ namespace lion.Views
         {
             if (e.SelectedItem == null)
                 return;
-            
+
             var feedDetails = e.SelectedItem as FeedDetails;
             await Navigation.PushAsync(new PostsDetailsPage(feedDetails));
             listViewFeed.SelectedItem = null;
@@ -21,36 +21,36 @@ namespace lion.Views
 
         void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
         {
-            listViewFeed.ItemsSource= GetPosts(e.NewTextValue);
+            listViewFeed.ItemsSource = GetPosts(e.NewTextValue);
         }
 
 
-        IEnumerable<FeedDetails> GetPosts(string searchText= null)
-		{
-			//Call to a remote service, placeholder for now
-			var posts= new List<FeedDetails>{
+        IEnumerable<FeedDetails> GetPosts(string searchText = null)
+        {
+            //Call to a remote service, placeholder for now
+            var posts = new List<FeedDetails>{
                 new FeedDetails {
-                    Post= "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. ", 
-                    UserUrl = "http://lorempixel.com/100/100/people/1"}
-                new FeedDetails { 
-                    Post = "And the only way to do great work is to love what you do. ", 
-                    UserUrl = "http://lorempixel.com/100/100/people/4", 
-                    Status= "Hiya!" } 
+                    Post = "And the only way to do great work is to love what you do. ",
+                    UserUrl = "https://lion.blob.core.windows.net/pic1/pic1.jpeg"},
+                new FeedDetails {
+                    Post = "And the only way to do great work is to love what you do. ",
+                    UserUrl = "https://lion.blob.core.windows.net/pic2/pic2.jpeg",
+                    Status= "Hiya!" }
             };
 
             if (String.IsNullOrWhiteSpace(searchText))
                 return posts;
-            
+
 
             return posts.Where(x => x.Post.StartsWith(searchText));
-		}
-		
-		void Handle_Refreshing(object sender, System.EventArgs e)
-		{
+        }
+
+        void Handle_Refreshing(object sender, System.EventArgs e)
+        {
             listViewFeed.ItemsSource = GetPosts();
 
             listViewFeed.EndRefresh();
-		}
+        }
 
         public HomeView()
         {
