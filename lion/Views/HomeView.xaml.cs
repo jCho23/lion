531a4378dfc2afb960d99852b9325e697da749f4
June <junecho.ucb@gmail.com>
2018-02-08 17:50:10 -0800

    updated URL

--- lion/Views/HomeView.xaml.cs
+++ lion/Views/HomeView.xaml.cs
@@ -29,8 +29,13 @@ namespace lion.Views
 		{
 			//Call to a remote service, placeholder for now
 			var posts= new List<FeedDetails>{
-                new FeedDetails {Post= "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. ", UserUrl = "http://lorempixel.com/100/100/people/1"},
-                new FeedDetails { Post = "And the only way to do great work is to love what you do. ", UserUrl = "http://lorempixel.com/100/100/people/4", Status= "Hiya!" } 
+                new FeedDetails {
+                    Post= "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. ", 
+                    UserUrl = "https://lion.blob.core.windows.net/pic1"},
+                new FeedDetails { 
+                    Post = "And the only way to do great work is to love what you do. ", 
+                    UserUrl = "http://lorempixel.com/100/100/people/4", 
+                    Status= "Hiya!" } 
             };
 
             if (String.IsNullOrWhiteSpace(searchText))
