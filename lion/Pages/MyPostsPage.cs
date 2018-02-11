ca39fc018ca378f6afbb930e9e2968fa9a8af8d7
June Cho <junecho.ucb@gmail.com>
2018-02-10 18:15:37 -0800

    refactored DMsPage

--- lion/Pages/MyPostsPage.cs
+++ lion/Pages/MyPostsPage.cs
@@ -5,9 +5,9 @@ using Xamarin.Forms;
 
 namespace lion.Pages
 {
-    public partial class DMsPage : ContentPage
+    public partial class MyPostsPage : ContentPage
     {
-        public DMsPage()
+        public MyPostsPage()
         {
             InitializeComponent();
         }
