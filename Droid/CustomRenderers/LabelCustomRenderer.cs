66803ab747b4406e1b36d91e2382774c96ceb8cc
June <junecho.ucb@gmail.com>
2018-02-13 15:57:11 -0800

    Added CustomRenderer

---
+++ Droid/CustomRenderers/LabelCustomRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using lion.Droid.CustomRenderers;
+using Plugin.CurrentActivity;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(LabelCustomRenderer))]
+namespace lion.Droid.CustomRenderers
+{
+    public class LabelCustomRenderer : LabelRenderer
+    {
+        #region Constructors
+        public LabelCustomRenderer(Context context) : base(context)
+        {
+        }
+        #endregion
+
+        #region Properties
+        Context CurrentContext => CrossCurrentActivity.Current.Activity;
+        #endregion
+
+        #region Methods
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+                Control.Typeface = Typeface.CreateFromAsset(CurrentContext.Assets, "SUPREME.ttf");
+        }
+        #endregion
+    }
+}
