91c5360a54b68a43953f6cff4e46c66d743bb9d9
June Cho <junecho.ucb@gmail.com>
2018-02-14 18:17:55 -0800

    added EntryLengthValidatorBehavior

--- lion/Pages/PostsDetailsPage.xaml.cs
+++ lion/Pages/PostsDetailsPage.xaml.cs
@@ -7,6 +7,9 @@ namespace lion.Pages
 {
     public partial class PostsDetailsPage : ContentPage
     {
+        public event EventHandler<Post> PostAdded;
+
+
         public PostsDetailsPage(FeedDetails feedDetails)
         {
             if (feedDetails == null)
@@ -16,5 +19,50 @@ namespace lion.Pages
 
             InitializeComponent();
         }
+
+        public class EntryLengthValidatorBehavior : Behavior<Entry>
+        {
+            public int MaxLength { get; set; }
+
+            protected override void OnAttachedTo(Entry bindable)
+            {
+                base.OnAttachedTo(bindable);
+                bindable.TextChanged += OnEntryTextChanged;
+            }
+
+            protected override void OnDetachingFrom(Entry bindable)
+            {
+                base.OnDetachingFrom(bindable);
+                bindable.TextChanged -= OnEntryTextChanged;
+            }
+
+            void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+            {
+                var entry = (Entry)sender;
+
+                // if Entry text is longer then valid length
+                if (entry.Text.Length > this.MaxLength)
+                {
+                    string entryText = entry.Text;
+
+                    entryText = entryText.Remove(entryText.Length - 1); // remove last char
+
+                    entry.Text = entryText;
+                }
+            }
+        }
+
+
+		void Handle_Completed(object sender, System.EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		
+		
+		
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
     }
 }
