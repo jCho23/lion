5391ae5bcc29be9f6ebd2ed5f6e5928cd33cc738
June <junecho.ucb@gmail.com>
2018-02-15 19:56:13 -0800

    Update ListView

--- lion/Pages/PostsDetailsPage.xaml.cs
+++ lion/Pages/PostsDetailsPage.xaml.cs
@@ -33,15 +33,15 @@ namespace lion.Pages
                     Status= "23 replies"},
                 new PostMessage {
                     Post = "Everything around you that you call life was made up by people that were no smarter than you",
-                    UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
+                    UserUrl = "https://cdn.inquisitr.com/wp-content/uploads/2016/04/kylie-jenner-paper-photoshoot.png",
                     Status= "8 replies" },
                 new PostMessage {
                     Post = "What's the best pizza joint around campus? ",
-                    UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
+                    UserUrl = "https://healthyceleb.com/wp-content/uploads/2016/07/Kevin-Hart-headshot.jpg",
                     Status= "48 replies" },
                 new PostMessage {
                     Post = "Everything around you that you call life was made up by people that were no smarter than you",
-                    UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
+                    UserUrl = "https://i.ytimg.com/vi/RnSgCv2X0ns/hqdefault.jpg",
                     Status= "8 replies" },
                 new PostMessage {
                     Post = "Everything around you that you call life was made up by people that were no smarter than you",
