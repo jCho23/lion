4522689875207f7e6f7f5987a5796c85769e9e1b
June <junecho.ucb@gmail.com>
2018-02-16 17:27:33 -0800

    Added TabbedPageCustomRenderer

---
+++ iOS/Custom Renderers/TabbedPageCustomRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using lion.iOS.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(TabbedPageCustomRenderer))]
+
+namespace lion.iOS.CustomRenderers
+{
+    public class TabbedPageCustomRenderer : TabbedRenderer
+
+    {
+        public TabbedPageCustomRenderer()
+        {
+            TabBar.TintColor = MonoTouch.UIKit.UIColor.Black;
+            TabBar.BarTintColor = MonoTouch.UIKit.UIColor.Blue;
+            TabBar.BackgroundColor = MonoTouch.UIKit.UIColor.Green;
+        }
+    }
+
+
+
+}
