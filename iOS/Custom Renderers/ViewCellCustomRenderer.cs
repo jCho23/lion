52a7bc2f54d9f9198892b5878557cc4153797435
June <junecho.ucb@gmail.com>
2018-02-17 16:10:48 -0800

    added ViewCellCustomRenderer

--- iOS/Custom Renderers/ViewCellCustomRenderer.cs
+++ iOS/Custom Renderers/ViewCellCustomRenderer.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using lion.iOS.CustomRenderers;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(ViewCell), typeof(ViewCellCustomRenderer))]
 namespace lion.iOS.CustomRenderers
 {
     public class ViewCellCustomRenderer
