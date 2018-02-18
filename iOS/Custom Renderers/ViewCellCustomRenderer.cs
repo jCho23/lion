4e2dce0471c44000c551194b65c0d41d0553e897
June <junecho.ucb@gmail.com>
2018-02-17 16:27:38 -0800

    added ViewCellCustomRenderer

--- iOS/Custom Renderers/ViewCellCustomRenderer.cs
+++ iOS/Custom Renderers/ViewCellCustomRenderer.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using lion.iOS.CustomRenderers;
+using UIKit;
 using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
 
 [assembly: ExportRenderer(typeof(ViewCell), typeof(ViewCellCustomRenderer))]
 namespace lion.iOS.CustomRenderers
 {
-    public class ViewCellCustomRenderer
+    public class ViewCellCustomRenderer : ViewCellRenderer
+
     {
-        public ViewCellCustomRenderer()
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
         {
+            var cell = base.GetCell(item, reusableCell, tv);
+            var view = item as ExtendedViewCell;
+            cell.SelectedBackgroundView = new UIView
+            {
+                BackgroundColor = view.SelectedBackgroundColor.ToUIColor(),
+            };
+
+            return cell;
         }
+
     }
 }
