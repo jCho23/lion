825ad0ca019d932d4c4495e3e69c5ecc9ef6fe2b
June <junecho.ucb@gmail.com>
2018-02-19 12:32:27 -0800

    cleanUP

--- lion/Pages/FeedAddPostModalPage.xaml.cs
+++ lion/Pages/FeedAddPostModalPage.xaml.cs
@@ -7,20 +7,6 @@ namespace lion.Pages
 {
     public partial class FeedAddPostModalPage : ContentPage
     {
-
-
-        //void Handle_Completed(object sender, System.EventArgs e)
-        //{
-        //    var app = Application.Current as App;
-        //    app.Post = ModalEditor.Text;
-        //}
-
-        //protected override void OnDisappearing()
-        //{
-        //    base.OnDisappearing();
-        //}
-
-
         async void Handle_Clicked(object sender, System.EventArgs e)
         {
             var homeView = new ContentPage();
@@ -33,8 +19,6 @@ namespace lion.Pages
 			InitializeComponent();
 
             BindingContext = Application.Current;
-
 		}
-		
     }
 }
