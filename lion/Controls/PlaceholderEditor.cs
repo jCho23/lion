a45489fdb12938ca137185643d468b86731111a4
June <junecho.ucb@gmail.com>
2018-02-19 15:01:19 -0800

    added Control

--- lion/Controls/PlaceholderEditor.cs
+++ lion/Controls/PlaceholderEditor.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using Xamarin.Forms;
+
 namespace lion.Controls
+
 {
-    public class PlaceholderEditor
+    public class PlaceholderEditor : Editor
     {
-        public PlaceholderEditor()
+        public static BindableProperty PlaceholderProperty
+            = BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(PlaceholderEditor));
+
+        public static BindableProperty PlaceholderColorProperty
+            = BindableProperty.Create(nameof(PlaceholderColor), typeof(Color), typeof(PlaceholderEditor), Color.Gray);
+
+        public string Placeholder
         {
+            get { return (string)GetValue(PlaceholderProperty); }
+            set { SetValue(PlaceholderProperty, value); }
+        }
+
+        public Color PlaceholderColor
+        {
+            get { return (Color)GetValue(PlaceholderColorProperty); }
+            set { SetValue(PlaceholderColorProperty, value); }
         }
     }
 }
