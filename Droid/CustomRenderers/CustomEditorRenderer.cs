4b83644d0992b2838d4d320b01c14f448c2b5dd5
June <junecho.ucb@gmail.com>
2018-02-19 15:54:30 -0800

    CleanUp

--- Droid/CustomRenderers/CustomEditorRenderer.cs
+++ Droid/CustomRenderers/CustomEditorRenderer.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
 namespace lion.Droid.CustomRenderers
 {
-    public class CustomEditorRenderer
-    {
-        public CustomEditorRenderer()
-        {
-        }
-    }
+    //public class CustomEditorRenderer : EditorRenderer
+    //{
+    //    public CustomEditorRenderer()
+    //    {
+
+    //    }
+
+    //    protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+    //    {
+    //        base.OnElementChanged(e);
+    //        if (e.NewElement != null)
+    //        {
+    //            var element = e.NewElement as CustomEditor;
+    //            this.Control.Hint = element.Placeholder;
+    //            Control.Gravity = Android.Views.GravityFlags.Start;
+    //            Control.SetBackgroundColor(global::Android.Graphics.Color.White);
+    //            Control.SetPadding(15, 15, 15, 15);
+    //        }
+    //    }
+
+    //}
 }
