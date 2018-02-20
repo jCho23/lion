10930a72c9201544c009e1c6d2108124b7251969
June <junecho.ucb@gmail.com>
2018-02-19 16:46:58 -0800

    fin SQlite Droid

--- Droid/Persistence/SQLiteDb.cs
+++ Droid/Persistence/SQLiteDb.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using System.IO;
+using lion.Droid.Persistence;
+using lion.Persistence;
+using SQLite;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDb))]
 namespace lion.Droid.Persistence
 {
-    public class SQLiteDb
+    public class SQLiteDb : ISQLiteDb
     {
-        public SQLiteDb()
+        public SQLiteAsyncConnection GetConnection()
         {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
         }
     }
 }
