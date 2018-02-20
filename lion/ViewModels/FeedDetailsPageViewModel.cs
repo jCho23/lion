f1920eb983d49ac5d8b57504bda8ea859590f44b
June <junecho.ucb@gmail.com>
2018-02-20 15:46:00 -0800

    Set Property MEthod

--- lion/ViewModels/FeedDetailsPageViewModel.cs
+++ lion/ViewModels/FeedDetailsPageViewModel.cs
@@ -3,35 +3,56 @@ using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace lion.ViewModels
 {
+    public class FeedDetailsPageViewModel : INotifyPropertyChanged
+    {
+        string replyPostInput = string.Empty;
+        ICommand postButtonCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ReplyPostInput
+        {
+            get => replyPostInput;
+            set => SetProperty(ref replyPostInput, value);
+        }
+
+        public ICommand PostButtonCommand => postButtonCommand ??
+            (postButtonCommand = new Command(async () => await ExecutePostButtonCommand()));
 
 
+        async Task ExecutePostButtonCommand()
+        {
+            //try
+            //{
+            //   
+            //}
+            //catch 
+            //{
+            //}
+            //finally
+            //{
+            //}
+        }
 
-    //public class FeedDetailsPageViewModel:INotifyPropertyChanged
-    //{
-    //    string replyPostInput = string.Empty;
-    //    ICommand postButtonCommand;
+        void SetProperty<T>(ref T backingStore, T value, Action onChanged = null, [CallerMemberName] string propertyname = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return;
 
-    //    public event PropertyChangedEventHandler PropertyChanged;
+            backingStore = value;
 
-    //    public ICommand PostButtonCommand => postButtonCommand ??
-    //        (postButtonCommand = new Command(async () => await ExecutePostButtonCommand()));
+            onChanged?.Invoke();
 
+            OnPropertyChanged(propertyname);
+        }
 
-    //    async Task ExecutePostButtonCommand()
-    //    {
-    //        //try
-    //        //{
-    //        //   
-    //        //}
-    //        //catch 
-    //        //{
-    //        //}
-    //        //finally
-    //        //{
-    //        //}
-    //    }
-    //}
+        void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
