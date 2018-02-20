251509c57a08041164259c2fddf53d33312b888d
June <junecho.ucb@gmail.com>
2018-02-19 16:45:15 -0800

    fin iOS SQlite

--- iOS/Persistence/SQLiteDb.cs
+++ iOS/Persistence/SQLiteDb.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.IO;
+using lion.iOS.Persistence;
+using lion.Persistence;
+using SQLite;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDb))]
 namespace lion.iOS.Persistence
 {
-    public class SQLiteDb
+    public class SQLiteDb : ISQLiteDb
     {
-        public SQLiteDb()
+        public SQLiteAsyncConnection GetConnection()
         {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
         }
     }
 }
+
