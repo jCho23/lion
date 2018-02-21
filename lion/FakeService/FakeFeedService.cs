0892cf6c106c70dae605d65653f3de6d763e0df9
June <junecho.ucb@gmail.com>
2018-02-21 10:31:50 -0800

    CleanUp

--- lion/FakeService/FakeFeedService.cs
+++ lion/FakeService/FakeFeedService.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using lion.Models;
+
 namespace lion.FakeService
 {
-    public class FakeFeedService
+    public static class FakeFeedService
     {
-        public FakeFeedService()
+        public static List<PostMessage> GetPostMessages()
         {
+            return new List<PostMessage>
+            {
+                new PostMessage {
+                    Post = "Everything around you that you call life was made up by people that were no smarter than you",
+                    UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
+                    Status= "8 replies" }
+            };
         }
     }
 }
