2dfda021ba7deb3866fad0c80e4edb75b9eb820f
June <junecho.ucb@gmail.com>
2018-02-24 09:49:36 -0800

    added PostMessageModel

--- lion/ViewModels/MyPostsViewModel.cs
+++ lion/ViewModels/MyPostsViewModel.cs
@@ -6,34 +6,42 @@ namespace lion.ViewModels
 {
     public class MyPostsViewModel : BaseViewModel
     {
-        ObservableCollection<ReplyMessageModel> myPostsListViewItemSource; 
+        ObservableCollection<PostMessageModel> myPostsListViewItemSource;
 
-        //IList<PostMessage> 
-        //MyPostsListView.ItemsSource = new List<PostMessage>{
-        //        new PostMessage {
-        //            Post = "And the only way to do great work is to love what you do. ",
-        //            UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
-        //            Status= "23 replies"},
-        //        new PostMessage {
-        //            Post = "Everything around you that you call life was made up by people that were no smarter than you",
-        //            UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
-        //            Status= "8 replies" },
-        //        new PostMessage {
-        //            Post = "What's the best pizza joint around campus? ",
-        //            UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
-        //            Status= "48 replies" },
-        //        new PostMessage {
-        //            Post = "Everything around you that you call life was made up by people that were no smarter than you",
-        //            UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
-        //            Status= "8 replies" },
-        //        new PostMessage {
-        //            Post = "Everything around you that you call life was made up by people that were no smarter than you",
-        //            UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
-        //            Status= "8 replies" },
-        //        new PostMessage {
-        //            Post = "Everything around you that you call life was made up by people that were no smarter than you",
-        //            UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
-        //            Status= "8 replies" }
-        //};
+        public MyPostsViewModel()
+        {
+            myPostsListViewItemSource = new ObservableCollection<PostMessageModel>
+            {
+                new PostMessageModel()
+                {
+                    PostText = "And the only way to do great work is to love what you do. ",
+                    PostUser= new UserProfileModel
+                    {
+                        PictureUrl="http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg"
+                    }
+
+                },
+                    //new PostMessage {
+                    //    Post = "Everything around you that you call life was made up by people that were no smarter than you",
+                    //    UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
+                    //    Status= "8 replies" },
+                    //new PostMessage {
+                    //    Post = "What's the best pizza joint around campus? ",
+                    //    UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
+                    //    Status= "48 replies" },
+                    //new PostMessage {
+                    //    Post = "Everything around you that you call life was made up by people that were no smarter than you",
+                    //    UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
+                    //    Status= "8 replies" },
+                    //new PostMessage {
+                    //    Post = "Everything around you that you call life was made up by people that were no smarter than you",
+                    //    UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
+                    //    Status= "8 replies" },
+                    //new PostMessage {
+                        //Post = "Everything around you that you call life was made up by people that were no smarter than you",
+                        //UserUrl = "http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg",
+                        //Status= "8 replies" }
+            };
+        }
     }
 }
