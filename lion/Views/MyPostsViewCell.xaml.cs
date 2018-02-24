894956d1d2dc2fca21402602b9450a0e2b6e3636
June <junecho.ucb@gmail.com>
2018-02-24 09:55:11 -0800

    added MyPostsViewCell

---
+++ lion/Views/MyPostsViewCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace lion.Views
+{
+    public partial class MyPostsViewCell : ContentPage
+    {
+        public MyPostsViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
