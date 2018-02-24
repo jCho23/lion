4e959579d0daa279dde88ddf207ce12134165e69
June <junecho.ucb@gmail.com>
2018-02-24 10:07:05 -0800

    Cleanup

--- lion/Views/MyPostsViewCell.xaml.cs
+++ lion/Views/MyPostsViewCell.xaml.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using lion.Models;
 using Xamarin.Forms;
 
 namespace lion.Views
 {
-    public partial class MyPostsViewCell : ContentPage
+    public partial class MyPostsViewCell : ViewCell
     {
         public MyPostsViewCell()
         {
             InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            var model = BindingContext as PostMessageModel;
+
+            MyPostsMessageLabel.Text = model.PostText;
+            UserProfilePicture.Source = model.PostUser.PictureUrl;
         }
     }
 }
