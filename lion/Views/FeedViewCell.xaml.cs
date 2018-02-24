645a6751014faa6afcca13bb4cfe0cb5b27ab5a5
June <junecho.ucb@gmail.com>
2018-02-23 16:47:45 -0800

    added FeedViewCell

---
+++ lion/Views/FeedViewCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace lion.Views
+{
+    public partial class FeedViewCell : ContentPage
+    {
+        public FeedViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
