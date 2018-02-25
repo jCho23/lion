fe2be4d31cbac8ecded300b9a7e8ea1db0dcde36
June <junecho.ucb@gmail.com>
2018-02-24 19:42:34 -0800

    added SplashPage

---
+++ lion/Pages/SplashPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+namespace lion.Pages
+{
+    public class SplashPage : ContentPage
+    {
+        Image splashImage;
+
+        public SplashPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            var splashLayout = new AbsoluteLayout();
+            splashImage = new Image
+            {
+                Source = "Bears.png",
+                WidthRequest = 100,
+                HeightRequest= 100
+            };
+                
+        }
+    }
+}
