47cc65b86084bc312f15ef128c335eb351f79ae1
June <junecho.ucb@gmail.com>
2018-02-25 09:34:44 -0800

    CleanUp

--- lion/Pages/ProfilePage.xaml.cs
+++ lion/Pages/ProfilePage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using lion.Models;
 using Xamarin.Forms;
 
 namespace lion.Pages
@@ -10,6 +10,17 @@ namespace lion.Pages
         public ProfilePage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            var model = BindingContext as PostMessageModel;
+
+          
+            UserProfilePicture.Source = model.PostUser.PictureUrl;
+            //PostNumberOfRepliesLabel. = model.NumberOfReplies;
         }
     }
 }
