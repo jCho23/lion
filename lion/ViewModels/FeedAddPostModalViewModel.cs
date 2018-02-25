5d776dc77a4f0d509f3bbd13d202a1bd347a37e6
June <junecho.ucb@gmail.com>
2018-02-24 19:09:39 -0800

    updated FeedAddPostModalViewModel()

--- lion/ViewModels/FeedAddPostModalViewModel.cs
+++ lion/ViewModels/FeedAddPostModalViewModel.cs
@@ -1,10 +1,47 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
 namespace lion.ViewModels
 {
-    public class FeedAddPostModalViewModel
+    public class FeedAddPostModalViewModel :BaseViewModel
     {
-        public FeedAddPostModalViewModel()
+        string userInputPost = string.Empty;
+
+        ICommand postMessageCommand;
+
+        public ICommand SubmitButtonCommand => postMessageCommand ??
+        (postMessageCommand = new Command(async () => await ExecutePostMessageCommand()));
+
+
+        public string UserInputPost
         {
+            get => userInputPost;
+            set => SetProperty(ref userInputPost, value);
+        }
+
+        async Task ExecutePostMessageCommand()
+        {
+            try
+            {
+                //var result = await UserInputPost;
+                //if (result == null)
+                {
+                    
+                }
+                else
+                {
+         
+                }
+
+            }
+           
+            catch (Exception e)
+            {
+                
+            }
+
         }
     }
 }
