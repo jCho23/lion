8f74ce41ddf670f0d5f786172f161fa8bd9debca
June <junecho.ucb@gmail.com>
2018-02-24 21:27:47 -0800

    CleanUp Event Handlers

--- lion/Pages/FeedAddPostModalPage.xaml.cs
+++ lion/Pages/FeedAddPostModalPage.xaml.cs
@@ -7,16 +7,16 @@ namespace lion.Pages
 {
     public partial class FeedAddPostModalPage : ContentPage
     {
-        async void Handle_Clicked(object sender, System.EventArgs e)
+        async void OnCanceled_Clicked(object sender, System.EventArgs e)
         {
             var homeView = new ContentPage();
 
             await Navigation.PushModalAsync(new NavigationPage(new Pages.LionTabbedPage()));
         }
 
-        void Handle_Completed(object sender, System.EventArgs e)
+        void OnSaveUserPost(object sender, System.EventArgs e)
         {
-            throw new NotImplementedException();
+            Application.Current.Properties["Post"] = ModalEditor.Text;
         }
 
         protected override void OnDisappearing()
