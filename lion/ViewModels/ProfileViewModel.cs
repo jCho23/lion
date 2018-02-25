3f6c78cdef901bb88492798071cdccf3108da4b5
June <junecho.ucb@gmail.com>
2018-02-25 11:47:14 -0800

    Added OC

--- lion/ViewModels/ProfileViewModel.cs
+++ lion/ViewModels/ProfileViewModel.cs
@@ -1,15 +1,89 @@
 ﻿using System;
 using Xamarin.Forms;
+using lion.Models;
+using System.Collections.ObjectModel;
+
 namespace lion.ViewModels
 {
     public class ProfileViewModel
     {
         //Image = 
+        ObservableCollection<PostMessageModel> myPostsListViewItemSource;
 
+        string postUserProfileUrl;
 
-
-        public ProfileViewModel()
+        public ProfileViewModel(PostMessageModel postMessage)
         {
+            myPostsListViewItemSource = new ObservableCollection<PostMessageModel>
+            {
+                new PostMessageModel()
+                {
+                    PostText = "And the only way to do great work is to love what you do. ",
+                    NumberOfReplies =23,
+                    PostUser= new UserProfileModel
+                    {
+                        PictureUrl="http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg"
+                    }
+                },
+
+                new PostMessageModel()
+                {
+                    PostText = "And the only way to do great work is to love what you do. ",
+                    NumberOfReplies =23,
+                    PostUser= new UserProfileModel
+                    {
+                        PictureUrl="http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg"
+                    }
+                },
+
+                new PostMessageModel()
+                {
+                    PostText = "And the only way to do great work is to love what you do. ",
+                    NumberOfReplies =23,
+                    PostUser= new UserProfileModel
+                    {
+                        PictureUrl="http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg"
+                    }
+                },
+
+                new PostMessageModel()
+                {
+                    PostText = "And the only way to do great work is to love what you do. ",
+                    NumberOfReplies =23,
+                    PostUser= new UserProfileModel
+                    {
+                        PictureUrl="http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg"
+                    }
+                },
+
+                new PostMessageModel()
+                {
+                    PostText = "And the only way to do great work is to love what you do. ",
+                    NumberOfReplies =23,
+                    PostUser= new UserProfileModel
+                    {
+                        PictureUrl="http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg"
+                    }
+                },
+
+                new PostMessageModel()
+                {
+                    PostText = "And the only way to do great work is to love what you do. ",
+                    NumberOfReplies =23,
+                    PostUser= new UserProfileModel
+                    {
+                        PictureUrl="http://www.profightdb.com/img/wrestlers/thumbs-600/6baee11272the-rock.jpg"
+                    }
+                }
+            };
+
+        }
+            
+
+        public string PostUserProfileUrl
+        {
+            get => postUserProfileUrl;
+            set => SetProperty(ref postUserProfileUrl, value);
         }
     }
 }
