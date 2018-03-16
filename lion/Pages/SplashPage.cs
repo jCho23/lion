3f5f28c61e736d1145bed1c1ebef4eb17772b3dd
June <junecho.ucb@gmail.com>
2018-03-15 20:23:10 -0400

    Added SplashPage

--- lion/Pages/SplashPage.cs
+++ lion/Pages/SplashPage.cs
@@ -1,42 +1,42 @@
-﻿//using System;
-//using Xamarin.Forms;
-//namespace lion.Pages
-//{
-//    public class SplashPage : ContentPage
-//    {
-//        Image splashImage;
+﻿using System;
+using Xamarin.Forms;
+namespace lion.Pages
+{
+    public class SplashPage : ContentPage
+    {
+        Image splashImage;
 
-//        public SplashPage()
-//        {
-//            NavigationPage.SetHasNavigationBar(this, false);
+        public SplashPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
 
-//            var splashLayout = new AbsoluteLayout();
-//            splashImage = new Image
-//            {
-//                Source = "Bears.png",
-//                WidthRequest = 100,
-//                HeightRequest = 100
-//            };
+            var splashLayout = new AbsoluteLayout();
+            splashImage = new Image
+            {
+                Source = "Bears.png",
+                WidthRequest = 100,
+                HeightRequest = 100
+            };
 
-//            AbsoluteLayout.SetLayoutFlags(splashImage, 
-//                AbsoluteLayoutFlags.PositionProportional);
-//            AbsoluteLayout.SetLayoutBounds(splashImage,
-//                new Rectangle(0.5,0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+            AbsoluteLayout.SetLayoutFlags(splashImage, 
+                AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(splashImage,
+                new Rectangle(0.5,0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
 
-//            splashLayout.Children.Add(splashImage);
+            splashLayout.Children.Add(splashImage);
 
-//            this.BackgroundColor = Color.Accent;
-//            this.Content = splashLayout;
-//        }
+            this.BackgroundColor = Color.Accent;
+            this.Content = splashLayout;
+        }
 
-//        protected override async void OnAppearing()
-//        {
-//            base.OnAppearing();
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
 
-//            await splashImage.ScaleTo(1, 2000);
-//            await splashImage.ScaleTo(0.5, 1500, Easing.Linear);
-//            await splashImage.ScaleTo(150, 1200, Easing.Linear);
-//            Application.Current.MainPage = new NavigationPage(new LionTabbedPage());
-//        }
-//    }
-//}
+            await splashImage.ScaleTo(1, 2000);
+            await splashImage.ScaleTo(0.5, 1500, Easing.Linear);
+            await splashImage.ScaleTo(150, 1200, Easing.Linear);
+            Application.Current.MainPage = new NavigationPage(new LionTabbedPage());
+        }
+    }
+}
