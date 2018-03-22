305fb250d005955a1cd9b75206ab50114e4f32df
June <junecho.ucb@gmail.com>
2018-03-22 11:57:05 -0500

    CleanUp SQLite

--- Droid/Persistence/SQLiteDb.cs
+++ Droid/Persistence/SQLiteDb.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.IO;
-using lion.Droid.Persistence;
-using lion.Persistence;
-using SQLite;
-using Xamarin.Forms;
+﻿//using System;
+//using System.IO;
+//using lion.Droid.Persistence;
+//using lion.Persistence;
+//using SQLite;
+//using Xamarin.Forms;
 
-[assembly: Dependency(typeof(SQLiteDb))]
-namespace lion.Droid.Persistence
-{
-    public class SQLiteDb : ISQLiteDb
-    {
-        public SQLiteAsyncConnection GetConnection()
-        {
-            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            var path = Path.Combine(documentsPath, "MySQLite.db3");
+//[assembly: Dependency(typeof(SQLiteDb))]
+//namespace lion.Droid.Persistence
+//{
+//    public class SQLiteDb : ISQLiteDb
+//    {
+//        public SQLiteAsyncConnection GetConnection()
+//        {
+//            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+//            var path = Path.Combine(documentsPath, "MySQLite.db3");
 
-            return new SQLiteAsyncConnection(path);
-        }
-    }
-}
+//            return new SQLiteAsyncConnection(path);
+//        }
+//    }
+//}
