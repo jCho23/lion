c424525a72d1d8e806887951cf5971cd891bdd68
June <junecho.ucb@gmail.com>
2018-03-25 10:53:05 -0700

    CleanUp

--- Droid/Persistence/SQLiteDb.cs
+++ Droid/Persistence/SQLiteDb.cs
@@ -1,9 +1,9 @@
-﻿//using System;
-//using System.IO;
+﻿using System;
+using System.IO;
 //using lion.Droid.Persistence;
 //using lion.Persistence;
-//using SQLite;
-//using Xamarin.Forms;
+using SQLite;
+using Xamarin.Forms;
 
 //[assembly: Dependency(typeof(SQLiteDb))]
 //namespace lion.Droid.Persistence
