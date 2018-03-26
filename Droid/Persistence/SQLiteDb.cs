7aad7df7bb591a0a40559a05a0c6caf53132e226
June <junecho.ucb@gmail.com>
2018-03-26 10:37:47 -0700

    CleanUp

--- Droid/Persistence/SQLiteDb.cs
+++ Droid/Persistence/SQLiteDb.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
-//using lion.Droid.Persistence;
-//using lion.Persistence;
+using lion.Droid.Persistence;
+using lion.Persistence;
 using SQLite;
 using Xamarin.Forms;
 
